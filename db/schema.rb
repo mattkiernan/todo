a106cee4a4b0f8dd356b9a3ffbb481a42145986d
mattkiernan <matt.kiernan@gmail.com>
2014-10-05 20:34:08 -0400

    Rebuild list schema to avoid conflict with old database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141005145932) do
+ActiveRecord::Schema.define(version: 20141005232615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
