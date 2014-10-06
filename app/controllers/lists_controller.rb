976c8d4e40431a93e7e14347d86d7be9ac637931
mattkiernan <matt.kiernan@gmail.com>
2014-10-05 20:31:45 -0400

    Add new, create & edit definitions for lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,25 @@
 class ListsController < ApplicationController
 
   def index
+    @lists = List.all
+  end
 
+  def new
+    @list = List.new
+    render :new
+  end
+
+  def create
+    List.create(list_params)
+    redirect_to root_path
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :description)
   end
 
 end
